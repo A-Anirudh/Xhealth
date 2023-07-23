bf925162088411c87cf6d230258cd3763a0c51f6
sakshamak <saksham.8484@gmail.com>
2023-07-24 04:00:06 +0530

    style: new signin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,7 @@
-import Header from "./components/header";
-import { Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { ToastContainer } from "react-toastify";
-import'react-toastify/dist/ReactToastify.css'
+
 const App = () =>{
   return (
     <>
-    <Header/>
-    <ToastContainer/>
-    <Container className="my-2">
-    <Outlet/>
-    </Container>
 
     </>
   )
--- frontend/src/components/PrivateRoutes.jsx
+++ frontend/src/components/PrivateRoutes.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import { Navigate,Outlet } from 'react-router-dom'
 import {  useSelector } from 'react-redux'
 
-const PrivateRoutes = () => {
+export const PrivateRoutes = () => {
 const{userInfo}=useSelector((state)=>state.auth)
 return userInfo? <Outlet/> :<Navigate to='/login_user' replace />//make it landing page later
 }
-
-export default PrivateRoutes
