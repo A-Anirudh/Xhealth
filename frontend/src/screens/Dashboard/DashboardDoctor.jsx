c48293ba4203c4712f309682453f7e99d932f891
Shreyas4603 <59449970+Shreyas4603@users.noreply.github.com>
2023-08-21 21:51:08 +0530

    API test

--- frontend/src/screens/Dashboard/DashboardDoctor.jsx
+++ frontend/src/screens/Dashboard/DashboardDoctor.jsx
@@ -1,8 +1,205 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Users } from '../../sdk/users';
+import { useTheme } from '@emotion/react';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import userProfile from "../../assets/profile.svg";
+import dropdown from "../../assets/dropdown.png";
+import coverImg from "../../assets/Doctors-pana 1.png";
+import boyimg from "../../assets/boyimg.png";
+import heart from "../../assets/heart.png";
+import bw from "../../assets/bw.png";
+import bp from "../../assets/bp.png";
+import gl from "../../assets/gl.png";
+import { Link } from "react-router-dom";
 
 export const DashboardDoctor = () => {
+  const theme = useTheme();
+	const user = new Users();
+  const[doctorInfo,refetchDoctor]= user.getDoctorInfo();
+  
+  useEffect(() => {
+     (async () => {
+      await refetchDoctor()
+    })
+  }, [])
+  
+
+
   return (
-    <div>Dashboard Doctor</div>
+    <Grid className="main-container" container
+    >
+			<Grid
+				item
+				xl={12}
+				margin="4rem 6rem 2rem"
+				container
+				justifyContent="space-around"
+				sx={{
+					[theme.breakpoints.down("lg")]: {
+						margin: "4rem 1rem 2rem"
+					},
+					[theme.breakpoints.down("sm")]: {
+						margin: "2rem 1rem"
+					},
+				}}
+			>
+				<Grid
+					item
+					xl={8}
+					boxShadow="0 4px 4px rgba(0,0,0,0.25)"
+					borderRadius={4}
+					overflow="hidden"
+					border={`4px solid ${theme.doctor.primary}`}
+				>
+					<Box
+						display="flex"
+						alignItems="center"
+						justifyContent="space-around"
+						padding="0 7rem"
+						sx={{
+							[theme.breakpoints.down("md")]: {
+								padding: "0",
+								width: "90vw"
+							},
+							[theme.breakpoints.down("sm")]: {
+								padding: "0",
+								// width: "initial"
+							},
+						}}
+					>
+						<Box
+							textAlign="center"
+							minWidth="28rem"
+							sx={{
+								[theme.breakpoints.down("lg")]: {
+									margin: "3rem",
+								},
+								[theme.breakpoints.down("sm")]: {
+									margin: "2rem 0",
+								},
+							}}
+						>
+							<Typography
+								variant="h3"
+								display="flex"
+								gap={2}
+								fontWeight="bold"
+								justifyContent="center"
+								alignItems="center"
+                fontFamily="Poppins"
+                fontSize="2.8rem"
+								sx={{
+									[theme.breakpoints.down("sm")]: {
+										fontSize: "2rem",
+										gap: 1
+									},
+									[theme.breakpoints.down("xsm")]: {
+										fontSize: "1.5rem"
+									},
+								}}
+							>
+								<Typography
+									variant="h3"
+									fontWeight="bold"
+                fontFamily="Poppins"
+
+									color={`${theme["purple-500"]}`}
+									sx={{
+										[theme.breakpoints.down("sm")]: {
+											fontSize: "2rem"
+										},
+										[theme.breakpoints.down("xsm")]: {
+											fontSize: "1.5rem"
+										},
+									}}
+								// onClick={() => {
+								//   appointments.find(item => {
+								//     if (item.status === "Scheduled" && new Date(item.appointmentDate) < new Date()) {
+								//       const data = {
+								//         _id: item._id,
+								//         newStatus: "Expired"
+								//       }
+								//       updateApt(data);
+								//     }
+								//   })
+								// }}
+								>
+									Welcome
+								</Typography>
+								Dr. {doctorInfo?.firstName}!
+							</Typography>
+							<Typography
+								variant="h5"
+                fontFamily="Poppins"
+
+								sx={{
+									display: "inline",
+									[theme.breakpoints.down("sm")]: {
+										fontSize: "1.2rem"
+									},
+									[theme.breakpoints.down("xsm")]: {
+										fontSize: "1rem"
+									},
+								}}
+							>Empowering Health, One Patient at a Time </Typography>
+						</Box>
+						<Box
+							sx={{
+								[theme.breakpoints.down("lg")]: {
+									display: "none"
+								},
+							}}
+						>
+							<img src={coverImg} alt="doctors image" />
+						</Box>
+					</Box>
+
+				</Grid>
+				<Grid item
+					xl={3}
+					backgroundColor="white"
+					padding="1rem 1.5rem"
+					borderRadius={4}
+					boxShadow="0 4px 4px rgba(0,0,0,0.25)"
+					position="relative"
+					sx={{
+						[theme.breakpoints.down("xl")]: {
+							display: "none"
+						},
+					}}
+				>
+                
+					<Typography variant="h5"fontFamily="Poppins" fontWeight={500} color={theme["blue-150"]} margin={0}>Profile</Typography>
+					<Link
+						style={{
+							alignItems: "center",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							top: "0",
+							height: "100%",
+							position: "absolute",
+							left: "0",
+							width: "100%",
+							textDecoration: "none"
+						}}
+						to="/profile-doctor"
+					>
+						<img src={boyimg} alt="user avatar" />
+                
+						
+            <Typography fontFamily="Poppins" color='black' fontSize='2rem' fontWeight='700'variant="h5">{doctorInfo?.firstName}</Typography>
+						<Typography fontFamily="Poppins" color={theme['gray-200']} fontSize='1rem'variant="h5">{doctorInfo?.department}</Typography>
+
+					</Link>
+				</Grid>
+			</Grid>
+
+
+      <Grid item xl={12} margin="4rem 6rem 2rem" borderRadius='1rem' backgroundColor={theme.doctor.background}>
+
+      </Grid>
+    </Grid>
   )
 }
 
