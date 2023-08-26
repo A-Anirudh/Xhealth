30e16edc02a239eb672e2c1f9d697137bcc0042e
A-Anirudh <aayanamanirudh@gmail.com>
2023-08-26 10:38:32 +0530

    changes

--- backend/controllers/healthRecordController.js
+++ backend/controllers/healthRecordController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler"
 import healthRecordModel from "../models/healthRecordModel.js";
 import User from "../models/userModel.js";
-
+import cos from "../config/cos.js";
 
 async function getHealthRecordInstance(email) {
     if (email) {
@@ -142,5 +142,45 @@ const newHealthRecord = asyncHandler(async (req, res) => {
 
 const getHealthRecordSpecific = asyncHandler(async (req, res) => { });
 
-export { getHealthRecordSpecific, getAllHealthRecords, newHealthRecord, getAllHealthRecordsAndroid };
 
+const storeDocument=(req,res)=>{
+    const {documentBase64,key}=req.body;
+        // console.log(documentBase64);
+        // console.log(key)
+            var params = {
+                Body: documentBase64.toString(),
+                Bucket: "cloud-object-storage-cos-standard-wwf", 
+                Key: key
+            };
+            cos.putObject(params, function(err, data) {
+                if (err) {
+                    console.log(err, err.stack); 
+                    res.status(401).json({status:"error",error:err,message:err.stack})
+                }          // an error occurred
+                else{     
+                    console.log(data); 
+                    res.status(200).json({status:"success",data})
+                }          // successful response
+            });            
+}
+
+
+const getDocument=(req,res)=>{
+    cos.getObject({Key:req.params.keyId,Bucket:"cloud-object-storage-cos-standard-wwf"})
+    .on('complete',(response)=>{
+        // Get the base64 encoded PDF from your server
+        pdfBase64="non"
+        if(response.data.Body)
+        var pdfBase64 = response.data.Body?.toString();
+        
+        // Set the content type and send the base64 encoded PDF as the response body
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(Buffer.from(pdfBase64, 'base64'));
+    }).on('error',(error)=>{
+        res.status(404).json(error)
+    })
+    .send()
+}
+
+
+export { getHealthRecordSpecific, getAllHealthRecords, newHealthRecord, getAllHealthRecordsAndroid,storeDocument,getDocument };
--- backend/routes/healthRecordsRouter.js
+++ backend/routes/healthRecordsRouter.js
@@ -1,7 +1,10 @@
 import express from "express"
 import { androidProtect, protect } from "../middleware/authMiddleware.js"
-import { getAllHealthRecords, getHealthRecordSpecific, newHealthRecord, getAllHealthRecordsAndroid } from "../controllers/healthRecordController.js";
+import { getAllHealthRecords, getHealthRecordSpecific, newHealthRecord, getAllHealthRecordsAndroid,getDocument,storeDocument } from "../controllers/healthRecordController.js";
 import { getUserProfile } from "../controllers/userController.js";
+
+
+
 
 const healthRecordsRouter = express.Router()
 //to get health records associated with a particular doctor
@@ -15,6 +18,8 @@ healthRecordsRouter.get('/doctor', (protect, getHealthRecordSpecific))
 healthRecordsRouter.route("/").post(protect, newHealthRecord)
 
 healthRecordsRouter.route("/getAll").post(protect, getAllHealthRecords)
+
+healthRecordsRouter.get("/key/:keyId",protect, getDocument).post("/",protect,storeDocument)
 
 //Dynamic access to doctors 
 //TODO: implement new protect as user may add doctors to allow permission to see their health records
