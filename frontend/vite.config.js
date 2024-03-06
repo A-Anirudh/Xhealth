f0d59d100c9c6df2aecb35fa8c09d400f2a57959
A-Anirudh <aayanamanirudh@gmail.com>
2024-03-07 00:23:12 +0530

    updated url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port:3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:5000/',
         changeOrigin: true,
         secure: false,
         ws: true,
